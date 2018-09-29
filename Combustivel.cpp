e31414d9cf68459d361755feb222d6f4edebee09
miltonalmeida <miltongustavo3@gmail.com>
2018-09-29 18:31:45 -0300

    veremos

--- Combustivel.cpp
+++ Combustivel.cpp
@@ -5,7 +5,7 @@
 
 void Combustivel::criacombustivel(float _x, float _y)
 {
-<<<<<<< HEAD
+
 	px1 = 0.1f + _x;
 	px2 = 0.1f + _x;
 	px3= 0.2f + _x;
@@ -19,27 +19,12 @@ void Combustivel::criacombustivel(float _x, float _y)
 	py4 = 0.9f + _y;
 	py5 = 0.8f + _y;
 	py6 = 0.4f + _y;
-
-=======
-	px1 = _px1;
-	px2 = _px2;
-	px3= _px3;
-	px4= _px4;
-	px5= _px5;
-	px6= _px6;
-
-	py1 = _py1;
-	py2 = _py2;
-	py3 = _py3;
-	py4 = _py4;
-	py5 = _py5;
-	py6 = _py6;
->>>>>>> parent of 3fd7416... mais um
+	
 }
 
 void Combustivel::desenhacobustivel()
 {
-<<<<<<< HEAD
+
 	if (paradowun)
 	{
 		glColor3f(1.0, 0.0, 0.0);
@@ -62,8 +47,6 @@ void Combustivel::movecombustivel(float _x, float _y)
 
 	if(paradowun)extremoUP = py1 - 0.4f;
 	else extremoUP = py3 + 0.9f;
-=======
->>>>>>> parent of 3fd7416... mais um
 
 	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_LINE_LOOP);
